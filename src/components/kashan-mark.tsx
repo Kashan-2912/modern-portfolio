744a6a7f44dd7a6c14aec94142e9bfa1a994fc5f
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-03-13 23:42:59 +0500

    feat: updated logo,

--- src/components/kashan-mark.tsx
+++ src/components/kashan-mark.tsx
@@ -3,18 +3,18 @@ export function SiteMark(props: React.ComponentProps<"svg">) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      viewBox="0 0 512 256"
+      viewBox="0 0 576 320"
       aria-hidden
       {...props}
     >
       <path
         fill="currentColor"
-        d="M0 0h64v256H0zM192 0h64v64h-64zM128 64h64v64h-64zM64 128h64v64h-64zM128 192h64v64h-64zM320 0h64v64h-64zM384 0h64v64h-64zM256 64h64v64h-64zM448 64h64v64h-64zM256 128h256v64h-256zM256 192h64v64h-64zM448 192h64v64h-64z"
+        d="M0 0h64v320H0zM192 0h64v64h-64zM128 64h64v64h-64zM64 128h64v64h-64zM128 192h64v64h-64zM192 256h64v64h-64zM384 0h64v64h-64zM448 0h64v64h-64zM320 64h64v64h-64zM512 64h64v64h-64zM320 128h256v64h-256zM320 192h64v64h-64zM512 192h64v64h-64zM320 256h64v64h-64zM512 256h64v64h-64z"
       />
     </svg>
   )
 }
 
 export function getMarkSVG(color: string) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 512 256"><path fill="${color}" d="M0 0h64v256H0zM192 0h64v64h-64zM128 64h64v64h-64zM64 128h64v64h-64zM128 192h64v64h-64zM320 0h64v64h-64zM384 0h64v64h-64zM256 64h64v64h-64zM448 64h64v64h-64zM256 128h256v64h-256zM256 192h64v64h-64zM448 192h64v64h-64z"/></svg>`
+  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 576 320"><path fill="${color}" d="M0 0h64v320H0zM192 0h64v64h-64zM128 64h64v64h-64zM64 128h64v64h-64zM128 192h64v64h-64zM192 256h64v64h-64zM384 0h64v64h-64zM448 0h64v64h-64zM320 64h64v64h-64zM512 64h64v64h-64zM320 128h256v64h-256zM320 192h64v64h-64zM512 192h64v64h-64zM320 256h64v64h-64zM512 256h64v64h-64z"/></svg>`
 }
